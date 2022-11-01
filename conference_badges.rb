15d29cd6297d323e2822ea47ae9b7e997f6f046b
Joe Robbins <joerobbins@Joes-MacBook-Air.local>
2022-11-01 09:00:40 -0400

    commit tree a76e27d120b20bddd100423961495593593e4b89

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+require 'pry'
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |person|
+        "Hello, my name is #{person}."
+    end
+end
+
+def assign_rooms(array)
+    array.map.with_index do |name, index|
+        "Hello, #{name}! You\'ll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer (array)
+    puts batch_badge_creator(array)
+    puts assign_rooms(array)
+    # assign_rooms
+end
+
+binding.pry
